1d6b6379795591e42111351c0433b34ccd3267e7
Mikhail Rogov <creynir@gmail.com>
2019-04-29 14:59:56 +0300

    Add support for Vue CLI 3 index.js - add return statement package.json - bump version README.md - add support flag

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: ['./src/index.js'],
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'build.js',
+        filename: 'vuex-socketio.js',
         library: ['VuexSocketio'],
         libraryTarget: 'umd'
     },
